945f692367cd355be15d76342aa6c1d9ad68eafc
NEEZH4CKS\nesto <nestorj.lopez@gmail.com>
2021-10-22 16:03:16 -0500

    Validacion de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faUserNurse, faTimes, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'quino';
   faCoffee = faCoffee;
+  faSignOutAlt = faSignOutAlt;
+  activeSesion;
+
+  constructor(private router: Router,) { 
+    
+  }
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token')){
+      this.activeSesion= true;
+    } else{
+      this.activeSesion= false;
+    }
+  }
+
+  singOut(){
+    localStorage.removeItem('token');
+    this.activeSesion= false;
+    this.router.navigateByUrl("/");
+  }
 }
