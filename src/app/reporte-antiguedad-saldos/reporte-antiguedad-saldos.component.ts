fe242edc81ef91518467ac50f7ebd41c32a56c87
jalh24 <jalh24@hotmail.com>
2022-04-06 12:55:30 -0500

    update

--- src/app/reporte-antiguedad-saldos/reporte-antiguedad-saldos.component.ts
+++ src/app/reporte-antiguedad-saldos/reporte-antiguedad-saldos.component.ts
@@ -16,9 +16,10 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
     })
   };
   deudores = [];
-  cincoDiasSumaTotal: number = 0;
-  diezDiasSumaTotal: number = 0;
+  unDiaSumaTotal: number = 0;
+  ochoDiasSumaTotal: number = 0;
   masQuinceDiasSumaTotal: number = 0;
+  granTotal: number = 0;
   tiposColaboradores: any[];
   faFileExcel = faFileExcel;
 
@@ -45,8 +46,8 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
     let arraySize = this.deudores.length;
     let precioSumaSubTotal = 0;
     let pagosSumaSubTotal = 0;
-    let cincoDiasSumaSubTotal = 0;
-    let diezDiasSumaSubTotal = 0;
+    let unDiaSumaSubTotal = 0;
+    let ochoDiasSumaSubTotal = 0;
     let masQuinceDiasSumaSubTotal = 0;
 
     this.deudores.forEach((element, index) => {
@@ -54,22 +55,22 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
       this.deudores[index].nombreTipoServicio = this.tiposColaboradores[Number(this.deudores[index].idTipoServicio)-1].nombre;
       this.deudores[index].fechaCreacion = new Date(this.deudores[index].fechaCreacion);
       this.deudores[index].fechaCreacion = this.deudores[index].fechaCreacion.toISOString().split('T')[0];
-      this.deudores[index]["pendienteCincoDias"] = 0;
-      this.deudores[index]["pendienteDiezDias"] = 0;
+      this.deudores[index]["pendienteUnDia"] = 0;
+      this.deudores[index]["pendienteOchoDias"] = 0;
       this.deudores[index]["pendienteMasQuinceDias"] = 0;
       this.deudores[index]["ultimo"] = false;
       this.deudores[index]["precioSubTotal"] = 0;
       this.deudores[index]["pagosSubTotal"] = 0;
-      this.deudores[index]["cincoDiasSubTotal"] = 0;
-      this.deudores[index]["diezDiasSubTotal"] = 0;
+      this.deudores[index]["unDiaSubTotal"] = 0;
+      this.deudores[index]["ochoDiasSubTotal"] = 0;
       this.deudores[index]["masQuinceDiasSubTotal"] = 0;
       this.deudores[index]["subTotal"] = false;
 
-      if (element.diferenciaDias >= 5 && element.diferenciaDias < 10) {
-        this.deudores[index].pendienteCincoDias = Number(this.deudores[index].cantidadPorPagar);
+      if (element.diferenciaDias >= 1 && element.diferenciaDias < 8) {
+        this.deudores[index].pendienteUnDia = Number(this.deudores[index].cantidadPorPagar);
       } else {
-        if (element.diferenciaDias >= 10 && element.diferenciaDias < 15) {
-          this.deudores[index].pendienteDiezDias = Number(this.deudores[index].cantidadPorPagar);
+        if (element.diferenciaDias >= 8 && element.diferenciaDias < 15) {
+          this.deudores[index].pendienteOchoDias = Number(this.deudores[index].cantidadPorPagar);
         } else {
           if (element.diferenciaDias >= 15) {
             this.deudores[index].pendienteMasQuinceDias = Number(this.deudores[index].cantidadPorPagar);
@@ -84,10 +85,10 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
           this.deudores[index].precioSubTotal = precioSumaSubTotal;
           pagosSumaSubTotal += Number(element.cantidadPagada);
           this.deudores[index].pagosSubTotal = pagosSumaSubTotal;
-          cincoDiasSumaSubTotal += Number(element.pendienteCincoDias);
-          this.deudores[index].cincoDiasSubTotal = cincoDiasSumaSubTotal;
-          diezDiasSumaSubTotal += Number(element.pendienteDiezDias);
-          this.deudores[index].diezDiasSubTotal = diezDiasSumaSubTotal;
+          unDiaSumaSubTotal += Number(element.pendienteUnDia);
+          this.deudores[index].unDiaSubTotal = unDiaSumaSubTotal;
+          ochoDiasSumaSubTotal += Number(element.pendienteOchoDias);
+          this.deudores[index].ochoDiasSubTotal = ochoDiasSumaSubTotal;
           masQuinceDiasSumaSubTotal += Number(element.pendienteMasQuinceDias);
           this.deudores[index].masQuinceDiasSubTotal = masQuinceDiasSumaSubTotal;
         } else {
@@ -95,21 +96,21 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
           this.deudores[index].precioSubTotal = precioSumaSubTotal;
           pagosSumaSubTotal += Number(element.cantidadPagada);
           this.deudores[index].pagosSubTotal = pagosSumaSubTotal;
-          cincoDiasSumaSubTotal += Number(element.pendienteCincoDias);
-          this.deudores[index].cincoDiasSubTotal = cincoDiasSumaSubTotal;
-          diezDiasSumaSubTotal += Number(element.pendienteDiezDias);
-          this.deudores[index].diezDiasSubTotal = diezDiasSumaSubTotal;
+          unDiaSumaSubTotal += Number(element.pendienteUnDia);
+          this.deudores[index].unDiaSubTotal = unDiaSumaSubTotal;
+          ochoDiasSumaSubTotal += Number(element.pendienteOchoDias);
+          this.deudores[index].ochoDiasSubTotal = ochoDiasSumaSubTotal;
           masQuinceDiasSumaSubTotal += Number(element.pendienteMasQuinceDias);
           this.deudores[index].masQuinceDiasSubTotal = masQuinceDiasSumaSubTotal;
           this.deudores[index].subTotal = true;
-          this.cincoDiasSumaTotal += cincoDiasSumaSubTotal;
-          this.diezDiasSumaTotal += diezDiasSumaSubTotal;
+          this.unDiaSumaTotal += unDiaSumaSubTotal;
+          this.ochoDiasSumaTotal += ochoDiasSumaSubTotal;
           this.masQuinceDiasSumaTotal += masQuinceDiasSumaSubTotal;
 
           precioSumaSubTotal = 0;
           pagosSumaSubTotal = 0;
-          cincoDiasSumaSubTotal = 0;
-          diezDiasSumaSubTotal = 0;
+          unDiaSumaSubTotal = 0;
+          ochoDiasSumaSubTotal = 0;
           masQuinceDiasSumaSubTotal = 0;
         }
       } else {
@@ -118,20 +119,21 @@ export class ReporteAntiguedadSaldosComponent implements OnInit {
           this.deudores[index].precioSubTotal = precioSumaSubTotal;
           pagosSumaSubTotal += Number(element.cantidadPagada);
           this.deudores[index].pagosSubTotal = pagosSumaSubTotal;
-          cincoDiasSumaSubTotal += Number(element.pendienteCincoDias);
-          this.deudores[index].cincoDiasSubTotal = cincoDiasSumaSubTotal;
-          diezDiasSumaSubTotal += Number(element.pendienteDiezDias);
-          this.deudores[index].diezDiasSubTotal = diezDiasSumaSubTotal;
+          unDiaSumaSubTotal += Number(element.pendienteUnDia);
+          this.deudores[index].unDiaSubTotal = unDiaSumaSubTotal;
+          ochoDiasSumaSubTotal += Number(element.pendienteOchoDias);
+          this.deudores[index].ochoDiasSubTotal = ochoDiasSumaSubTotal;
           masQuinceDiasSumaSubTotal += Number(element.pendienteMasQuinceDias);
           this.deudores[index].masQuinceDiasSubTotal = masQuinceDiasSumaSubTotal;
           this.deudores[index].subTotal = true;
-          this.cincoDiasSumaTotal += cincoDiasSumaSubTotal;
-          this.diezDiasSumaTotal += diezDiasSumaSubTotal;
+          this.unDiaSumaTotal += unDiaSumaSubTotal;
+          this.ochoDiasSumaTotal += ochoDiasSumaSubTotal;
           this.masQuinceDiasSumaTotal += masQuinceDiasSumaSubTotal;
 
       }
 
     });
+    this.granTotal = this.unDiaSumaTotal + this.ochoDiasSumaTotal + this.masQuinceDiasSumaTotal;
   }
 
   public exportTableToExcel(): void {
